87b821eb7608d0a652e2fdeb69619e8df889ec54
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-07-20 21:45:06 +0530

    Added more options to button

--- src/app/components/native-sdk/button/button.component.ts
+++ src/app/components/native-sdk/button/button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { NativeSDKExceptionService } from "../../../services/public/native-sdk/exception.service";
 
 @Component({
   selector: "native-button",
@@ -6,15 +7,28 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent implements OnInit {
-  X: number = 0
-  Y: number = 0
+  X: number = 0;
+  Y: number = 0;
 
-  constructor() {}
+  @Input("isOutlined") isOutlined: boolean = false;
+  @Input("isDisabled") isDisabled: boolean = false;
+  @Input("innerComponent") innerComponent: HTMLElement;
+  @Input("innerText") innerText: string = "";
+  @Input("overflow") overflow: "clip" | "ellipsis";
+  @Input("width") width: number;
+  @Input("height") height: number;
+
+  constructor(private exceptionService: NativeSDKExceptionService) {}
 
   move($event: MouseEvent) {
     this.X = $event.clientX;
     this.Y = $event.clientY;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.innerText && this.innerComponent)
+      this.exceptionService.AssertionException(
+        `Expected data inside button for rendering got "null"`
+      );
+  }
 }
