bb6d2718871181dbd300ad91dbeb3911df8a4392
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-07-20 12:28:29 +0530

    Minor changes

--- src/app/components/native-sdk/button/button.component.ts
+++ src/app/components/native-sdk/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'native-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
