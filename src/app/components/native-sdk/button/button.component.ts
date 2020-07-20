f0bea46a08fa8a202a222527d76a8391d59170cc
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-07-20 13:16:32 +0530

    Added button

--- src/app/components/native-sdk/button/button.component.ts
+++ src/app/components/native-sdk/button/button.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'native-button',
-  templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  selector: "native-button",
+  templateUrl: "./button.component.html",
+  styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent implements OnInit {
+  X: number = 0
+  Y: number = 0
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  move($event: MouseEvent) {
+    this.X = $event.clientX;
+    this.Y = $event.clientY;
   }
 
+  ngOnInit(): void {}
 }
