81d0711f71955e854a3694d24462a5f786f1fab7
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-07-21 13:16:40 +0530

    Added dynamic title change desktop

--- src/app/routes/global/desktop/desktop.component.ts
+++ src/app/routes/global/desktop/desktop.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { TopbarPanelService } from "src/app/services/public/api/panel/topbar/topbar.service";
 
 @Component({
-  selector: 'app-desktop',
-  templateUrl: './desktop.component.html',
-  styleUrls: ['./desktop.component.scss']
+  selector: "app-desktop",
+  templateUrl: "./desktop.component.html",
+  styleUrls: ["./desktop.component.scss"],
 })
 export class DesktopComponent implements OnInit {
+  constructor(private topbarPanelService: TopbarPanelService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  desktopFocused() {
+    console.log("fdjo");
+    this.topbarPanelService.title.next("CyanideOS");
   }
 
+  ngOnInit(): void {}
 }
