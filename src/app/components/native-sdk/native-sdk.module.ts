e00084cad52aaf1b8ed51c1d61a5058ada9dfdd8
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-07-21 10:50:38 +0530

    Added checkbox

--- src/app/components/native-sdk/native-sdk.module.ts
+++ src/app/components/native-sdk/native-sdk.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { NativeSdkComponent } from "./native-sdk.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { ButtonComponent } from "./button/button.component";
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
-  declarations: [NativeSdkComponent, ButtonComponent],
+  declarations: [NativeSdkComponent, ButtonComponent, CheckboxComponent],
   imports: [BrowserModule, CommonModule],
   exports: [NativeSdkComponent],
 })
