db5be4da391f5ad404549f2f0ce328be1dc9972e
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-08-05 23:11:17 +0530

    Changed wallpaper

--- src/app/services/private/user/user.service.ts
+++ src/app/services/private/user/user.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from "rxjs";
 })
 export class UserService {
   wallpaper: BehaviorSubject<string> = new BehaviorSubject(
-    "/assets/wallpaper.jpg"
+    "/assets/wallpapers/autmn.jpg"
   );
 
   constructor() {}
