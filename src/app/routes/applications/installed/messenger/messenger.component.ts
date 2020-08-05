dff1d9897104aef2651a689febea3bb926bf2cc1
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-08-05 22:56:39 +0530

    Styled notifications

--- src/app/routes/applications/installed/messenger/messenger.component.ts
+++ src/app/routes/applications/installed/messenger/messenger.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-messenger',
-  templateUrl: './messenger.component.html',
-  styleUrls: ['./messenger.component.scss']
+  selector: "ms-messenger-microsoft-com",
+  templateUrl: "./messenger.component.html",
+  styleUrls: ["./messenger.component.scss"],
 })
 export class MessengerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
