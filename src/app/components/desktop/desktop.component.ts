3ecdf6dcb4413a5b8ab2744efc9160ef49433f3d
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-09-25 12:19:50 +0530

    Added content to Desktop component

--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WindowsManagerService } from 'src/app/native-sdk/services/window-manager/window-manager.service';
+import { TopPanelManagerService } from 'src/app/services/panel/top-panel-manager/top-panel-manager.service';
 
 @Component({
   selector: 'Desktop',
@@ -6,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desktop.component.scss'],
 })
 export class DesktopComponent implements OnInit {
-  constructor() {}
+  constructor(
+    protected topPanelManagerService: TopPanelManagerService,
+    protected windowsManagerService: WindowsManagerService
+  ) {}
+
+  desktopFocused() {
+    this.topPanelManagerService.title.next('CyanideOS');
+  }
+
+  openNewWindow() {
+    this.windowsManagerService.openWindowByPackageName(
+      'ms-outlook.microsoft.com'
+    );
+  }
 
   ngOnInit(): void {}
 }
